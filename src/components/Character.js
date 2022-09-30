782d1434388693e7820111a82a554d6578bc4516
Xoerte <jaimefabri037@gmail.com>
2022-09-29 21:09:19 -0300

    primer commit

---
+++ src/components/Character.js
@@ -0,0 +1,11 @@
+const Character = ({character}) => {
+	return(
+		 <div className="text-center p-5">
+           <h3>{character.name}</h3>
+           <img src={character.image} className="img-fluid rounded-pill"/>
+           <p>{character.origin.name}</p>
+         </div>
+	
+		)
+}
+export default Character
