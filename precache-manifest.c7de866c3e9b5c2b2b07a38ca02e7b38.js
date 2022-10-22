33142ff7fdc18817c4eba76c2a1c13e9ebdfbecf
Xoerte <jaimefabri037@gmail.com>
2022-10-22 19:53:27 -0300

    Updates

---
+++ precache-manifest.c7de866c3e9b5c2b2b07a38ca02e7b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb4fb60e3ca22fbc9604",
+    "url": "/randomWors-react/static/css/main.dc8a3a80.chunk.css"
+  },
+  {
+    "revision": "bb4fb60e3ca22fbc9604",
+    "url": "/randomWors-react/static/js/main.bb4fb60e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/randomWors-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "56988f7f7c635cb8f9da",
+    "url": "/randomWors-react/static/js/2.56988f7f.chunk.js"
+  },
+  {
+    "revision": "763c4db247de564ab6bb",
+    "url": "/randomWors-react/static/js/runtime~main.763c4db2.js"
+  },
+  {
+    "revision": "87fe4a7e6936fea689675c6721d1cd07",
+    "url": "/randomWors-react/index.html"
+  }
+];
